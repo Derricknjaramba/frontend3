d69183c4007d1371699ea5098eb347a88503de58
Derricknjaramba <njarambaderrick@gmail.com>
2024-09-22 22:34:05 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust according to your file types
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
